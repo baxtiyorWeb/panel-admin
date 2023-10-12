4f7498789d9ca2b14c455d3149bed1356e14c96e
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2023-10-12 14:52:42 +0300

    ok

--- src/components/pages/reports/ExpenseReports.jsx
+++ src/components/pages/reports/ExpenseReports.jsx
@@ -1,12 +1,12 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {Courses_time} from "../../progress/data";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Courses_time } from "../../progress/data";
 
 export const ExpensesReports = () => {
 	const [search, setSearch] = useState("");
 	return (
 		<>
-			
+
 			<div className="chart-progress dark:bg-[#353c48]">
 				<div className="add-link">
 					<h1>Fee reports List</h1>
@@ -24,7 +24,7 @@ export const ExpensesReports = () => {
 					</div>
 					<div className="user_input">
 						<h4>Search:</h4>
-						<input type="text" className={'dark:bg-[#3B4452] border border-cyan-600'} onChange={(e) => setSearch(e.target.value)}/>
+						<input type="text" className={'dark:bg-[#3B4452] border border-cyan-600'} onChange={(e) => setSearch(e.target.value)} />
 					</div>
 				</div>
 				<div id="demo">
@@ -35,43 +35,43 @@ export const ExpensesReports = () => {
 								className="table table-hover table-mc-light-blue"
 							>
 								<thead>
-								<tr>
-									<th>#</th>
-									<th>Course Title</th>
-									<th>Category</th>
-									<th>Duration</th>
-									<th>Fee</th>
-									<th>Students</th>
-									<th>Faculties</th>
-									<th>Batches</th>
-									<th>Email Status</th>
-									<th>Action</th>
-								</tr>
+									<tr>
+										<th>#</th>
+										<th>Course Title</th>
+										<th>Category</th>
+										<th>Duration</th>
+										<th>Fee</th>
+										<th>Students</th>
+										<th>Faculties</th>
+										<th>Batches</th>
+										<th>Email Status</th>
+										<th>Action</th>
+									</tr>
 								</thead>
 								<tbody>
-								{Courses_time.filter((users) =>
-									users.title.toLowerCase().includes(search)
-								).map((item) => {
-									return (
-										<tr key={item.id} className={'even:dark:bg-[#313843]  even:hover:bg-[#E7E9EB] dark:bg-[#353C48] text-[#398dc9] dark:text-[#EEE8CC] font-normal'}>
-											<td>{item.id}</td>
-											<td>
-												<Link to={"#"}>{item.link}</Link>
-											</td>
-											<td>{item.title}</td>
-											<td>{item.students}</td>
-											<td>{item.students_progress}</td>
-											<td>{item.star}</td>
-											<td>{item.freeCollected}</td>
-											<td>{item.number}</td>
-											<td>{item.email_status}</td>
-											<td className={'td_flex'}>
-												<span className="icons">{<item.edit/>}</span>
-												<span className="icons">{<item.delete/>}</span>
-											</td>
-										</tr>
-									);
-								})}
+									{Courses_time.filter((users) =>
+										users.title.toLowerCase().includes(search)
+									).map((item) => {
+										return (
+											<tr key={item.id} className={'even:dark:bg-[#313843]  even:hover:bg-[#E7E9EB] dark:bg-[#353C48] text-[#398dc9] dark:text-[#EEE8CC] font-normal'}>
+												<td>{item.id}</td>
+												<td>
+													<Link to={"#"}>{item.link}</Link>
+												</td>
+												<td>{item.title}</td>
+												<td>{item.students}</td>
+												<td>{item.students_progress}</td>
+												<td>{item.star}</td>
+												<td>{item.freeCollected}</td>
+												<td>{item.number}</td>
+												<td>{item.email_status}</td>
+												<td className={'td_flex'}>
+													<span className="icons">{<item.edit />}</span>
+													<span className="icons">{<item.delete />}</span>
+												</td>
+											</tr>
+										);
+									})}
 								</tbody>
 							</table>
 						</div>
