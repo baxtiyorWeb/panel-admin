acda17f73031155dd8feb6157d2aaa67506485d9
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2023-11-11 19:30:55 +0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import './index.css'
-import {ProSidebarProvider} from "react-pro-sidebar";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { ProSidebarProvider } from "react-pro-sidebar";
 import ThemeProvider from "./components/context/ThemeProvider.jsx";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
-import {ToastContainer} from "react-toastify";
-
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
-    <ThemeProvider storageKey={'vite-theme'}>
-        <BrowserRouter>
-            <ToastContainer position={"top-left"} autoClose={3000} hideProgressBar={false} newestOnTop={false}
-                            closeOnClick pauseOnFocusLoss draggable={true}/>
-            <ProSidebarProvider>
-                <DevSupport ComponentPreviews={ComponentPreviews}
-                            useInitialHook={useInitial}
-                >
-                    <App/>
-                </DevSupport>
-            </ProSidebarProvider>
-        </BrowserRouter>
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev/index.js";
+import { ToastContainer } from "react-toastify";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider storageKey={"vite-theme"}>
+      <BrowserRouter>
+        <ToastContainer
+          position={"top-left"}
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnFocusLoss
+          draggable={true}
+        />
+        <ProSidebarProvider>
+          <DevSupport
+            ComponentPreviews={ComponentPreviews}
+            useInitialHook={useInitial}
+          >
+            <App />
+          </DevSupport>
+        </ProSidebarProvider>
+      </BrowserRouter>
     </ThemeProvider>
-</React.StrictMode>,)
+  </React.StrictMode>
+);
